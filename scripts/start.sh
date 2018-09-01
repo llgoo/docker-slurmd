12fb795b2df0dbc529daf8897e540f14d2398184
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2018-09-01 22:02:24 +0700

    incorrect slurm.conf path when sed

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,10 +7,11 @@ if [ -z ${SLURM_BASE_FQDN+x} ]; then echo "SLURM_NODE_NAMES not set" && exit 1; 
 #if [ -z ${INFLUXDB_HOST+x} ]; then echo "INFLUXDB_HOST not set" && exit 1; fi
 #if [ -z ${INFLUXDB_DATABASE_NAME+x} ]; then echo "INFLUXDB_DATABASE_NAME not set" && exit 1; fi
 
-sed -i -e "s/###SLURM_CLUSTER_NAME###/${SLURM_CLUSTER_NAME}/g" /usr/local/etc/slurm.conf
-sed -i -e "s/###SLURM_CONTROL_MACHINE###/${SLURM_CONTROL_MACHINE}/g" /usr/local/etc/slurm.conf
-sed -i -e "s/###SLURM_NODE_NAMES###/${SLURM_NODE_NAMES}/g" /usr/local/etc/slurm.conf
-sed -i -e "s/###SLURM_BASE_FQDN###/${SLURM_BASE_FQDN}/g" /usr/local/etc/slurm.conf
+SLURM_CONF_LOCATION=/etc/slurm/slurm.conf
+sed -i -e "s/###SLURM_CLUSTER_NAME###/${SLURM_CLUSTER_NAME}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_CONTROL_MACHINE###/${SLURM_CONTROL_MACHINE}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_NODE_NAMES###/${SLURM_NODE_NAMES}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_BASE_FQDN###/${SLURM_BASE_FQDN}/g" $SLURM_CONF_LOCATION
 #sed -i -e "s/###INFLUXDB_HOST###/${INFLUXDB_HOST}/g" /usr/local/etc/acct_gather.conf
 #sed -i -e "s/###INFLUXDB_DATABASE_NAME###/${INFLUXDB_DATABASE_NAME}/g" /usr/local/etc/acct_gather.conf
 
