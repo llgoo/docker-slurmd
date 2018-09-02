31520426db7f8221c1e5ba45e99f88406cc4be1a
Krittin Phornsiricharoenphant <oatkrittin@gmail.com>
2018-09-02 23:03:47 +0700

    decouple NODE_NAME AND NODE_ADDR as naturally

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,16 +2,18 @@
 
 if [ -z ${SLURM_CLUSTER_NAME+x} ]; then echo "SLURM_CLUSTER_NAME not set" && exit 1; fi
 if [ -z ${SLURM_CONTROL_MACHINE+x} ]; then echo "SLURM_CONTROL_MACHINE not set" && exit 1; fi
-if [ -z ${SLURM_NODE_NAMES+x} ]; then echo "SLURM_NODE_NAMES not set" && exit 1; fi
-if [ -z ${SLURM_BASE_FQDN+x} ]; then echo "SLURM_NODE_NAMES not set" && exit 1; fi
+if [ -z ${SLURM_CONTROL_ADDR+x} ]; then echo "SLURM_CONTROL_ADDR not set" && exit 1; fi
+if [ -z ${SLURM_NODE_NAME+x} ]; then echo "SLURM_NODE_NAME not set" && exit 1; fi
+if [ -z ${SLURM_NODE_ADDR+x} ]; then echo "SLURM_NODE_ADDR not set" && exit 1; fi
 #if [ -z ${INFLUXDB_HOST+x} ]; then echo "INFLUXDB_HOST not set" && exit 1; fi
 #if [ -z ${INFLUXDB_DATABASE_NAME+x} ]; then echo "INFLUXDB_DATABASE_NAME not set" && exit 1; fi
 
 SLURM_CONF_LOCATION=/etc/slurm/slurm.conf
 sed -i -e "s/###SLURM_CLUSTER_NAME###/${SLURM_CLUSTER_NAME}/g" $SLURM_CONF_LOCATION
 sed -i -e "s/###SLURM_CONTROL_MACHINE###/${SLURM_CONTROL_MACHINE}/g" $SLURM_CONF_LOCATION
-sed -i -e "s/###SLURM_NODE_NAMES###/${SLURM_NODE_NAMES}/g" $SLURM_CONF_LOCATION
-sed -i -e "s/###SLURM_BASE_FQDN###/${SLURM_BASE_FQDN}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_CONTROL_ADDR###/${SLURM_CONTROL_ADDR}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_NODE_NAME###/${SLURM_NODE_NAME}/g" $SLURM_CONF_LOCATION
+sed -i -e "s/###SLURM_NODE_ADDR###/${SLURM_NODE_ADDR}/g" $SLURM_CONF_LOCATION
 #sed -i -e "s/###INFLUXDB_HOST###/${INFLUXDB_HOST}/g" /usr/local/etc/acct_gather.conf
 #sed -i -e "s/###INFLUXDB_DATABASE_NAME###/${INFLUXDB_DATABASE_NAME}/g" /usr/local/etc/acct_gather.conf
 
